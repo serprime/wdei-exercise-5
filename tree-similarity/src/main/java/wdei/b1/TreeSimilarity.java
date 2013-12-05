072803e5f329449d605946941cb38b7a26669bd5
serprime <philipp.a.steinwender@gmail.com>
2013-12-05 20:09:38 +0100

    added tree similarity project

---
+++ tree-similarity/src/main/java/wdei/b1/TreeSimilarity.java
@@ -0,0 +1,18 @@
+package wdei.b1;
+
+import org.jsoup.nodes.Document;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: serprime
+ * Date: 05.12.13
+ * Time: 19:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class TreeSimilarity {
+
+    public double calculate(Document left, Document right) {
+        return 0f;
+    }
+
+}
---
+++ tree-similarity/src/test/java/wdei/b1/TreeSimilarityTest.java
@@ -0,0 +1,30 @@
+package wdei.b1;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: serprime
+ * Date: 05.12.13
+ * Time: 19:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class TreeSimilarityTest {
+
+    @Test
+    public void testDocs() {
+        String left = "" +
+                "<html><h1>Some headline</h1><p>and a paragraph</p></html>";
+        String right = "" +
+                "<html><h1>Some headline</h1><p>and a paragraph</p></html>";
+
+        Document leftDoc = Jsoup.parse(left);
+        Document rightDoc = Jsoup.parse(right);
+
+        System.out.println("tree distance: " + new TreeSimilarity().calculate(leftDoc, rightDoc));
+
+    }
+
+}
