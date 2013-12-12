9b810e0cf2e71d5aac7d37e179961ccf033491a4
serprime <philipp.a.steinwender@gmail.com>
2013-12-12 22:02:08 +0100

    implemented stephans html-path-jaro-similarity

--- tree-similarity/src/main/java/wdei/b1/TreeSimilarity.java
+++ tree-similarity/src/main/java/wdei/b1/TreeSimilarity.java
@@ -2,74 +2,28 @@ package wdei.b1;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-/**
- * Created with IntelliJ IDEA.
- * User: serprime
- * Date: 05.12.13
- * Time: 19:34
- * To change this template use File | Settings | File Templates.
- */
 public class TreeSimilarity {
+
+    private JaroDistance jaroDistance = new JaroDistance();
 
     public double calculate(Document left, Document right) {
         Element leftRoot = left.select("html").iterator().next();
+        StringBuilder l = new StringBuilder();
+        getPath(leftRoot, l);
+
         Element rightRoot = right.select("html").iterator().next();
+        StringBuilder r = new StringBuilder();
+        getPath(rightRoot, r);
 
-        return stm(leftRoot, rightRoot);
+        return jaroDistance.calculate(l.toString(), r.toString());
     }
 
-    private int stm(Element leftRoot, Element rightRoot) {
-        if (!same(leftRoot, rightRoot)) {
-            return 0;
+    private void getPath(Element tree, StringBuilder str) {
+        str.append(tree.nodeName());
+        for (Element element : tree.children()) {
+            getPath(element, str);
         }
-
-
-        Element[] leftChildren = getChildren(leftRoot);
-        Element[] rightChildren = getChildren(rightRoot);
-        int[][] M = new int[leftChildren.length + 1][rightChildren.length + 1];
-
-        for (int i = 1; i <= leftChildren.length; i++) {
-            for (int j = 1; j <= rightChildren.length; j++) {
-                int a = M[i][j - 1];
-                int b = M[i - 1][j];
-                int c = M[i - 1][j - 1];
-                int z = stm(leftChildren[i-1], rightChildren[j-1]);
-                M[i][j] = max(
-                        a,
-                        b,
-                        c + z
-                );
-            }
-        }
-        return M[leftChildren.length][rightChildren.length] + 1;
-    }
-
-    private int max(int... vals) {
-        int max = Integer.MIN_VALUE;
-        for (int val : vals) {
-            if (val > max) {
-                max = val;
-            }
-        }
-        return max;
-    }
-
-    // convert Elements to an array
-    private Element[] getChildren(Element element) {
-        Element[] children = new Element[element.children().size()];
-        int i = 0;
-        for (Element child : element.children()) {
-            children[i] = child;
-            i++;
-        }
-        return children;
-    }
-
-    private boolean same(Element left, Element right) {
-        System.out.println(left.nodeName() + ":" + right.nodeName() + " = " + left.nodeName().equals(right.nodeName()));
-        return left.nodeName().equals(right.nodeName());
     }
 
 }
--- tree-similarity/src/test/java/wdei/b1/TreeSimilarityTest.java
+++ tree-similarity/src/test/java/wdei/b1/TreeSimilarityTest.java
@@ -1,8 +1,12 @@
 package wdei.b1;
 
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,16 +19,47 @@ public class TreeSimilarityTest {
 
     @Test
     public void testDocs() {
-        String left = "" +
-                "<html><h1>Some headline</h1><p>and a paragraph</p></html>";
-        String right = "" +
-                "<html><h1>Some headline</h1><p>and a paragraph</p></html>";
 
+        String left = "<html><h1></h1><p></p></html>";
+        String right = "<html><h1></h1><p></p></html>";
+
+        test(left, right);
+
+        left = "<html>" +
+                "<h1></h1>" +
+                "  <div>" +
+                "    <div>" +
+                "      <p></p><span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p><span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p><span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p><span></span>" +
+                "    </div>" +
+                "  </div>" +
+                "</html>";
+        right = "<html><h1></h1><p></p></html>";
+
+        test(left, right);
+
+    }
+
+    private void test(String left, String right) {
         Document leftDoc = Jsoup.parse(left);
         Document rightDoc = Jsoup.parse(right);
-
+        System.out.println("\n\n TEST");
         System.out.println("node matches: " + new TreeSimilarity().calculate(leftDoc, rightDoc));
+    }
 
+    @Test
+    public void testWikiPages() throws IOException {
+        String l = FileUtils.readFileToString(new File("src/test/resources/wikiFoo.html"));
+        String r = FileUtils.readFileToString(new File("src/test/resources/wikiJaroWinkler.html"));
+        test(l, r);
     }
 
 }
