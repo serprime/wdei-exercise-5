6194ba3cd3170ee2b0df55c59ece8d09c64c638b
serprime <philipp.a.steinwender@gmail.com>
2013-12-12 22:35:56 +0100

    added node labelling for tagnames

--- tree-similarity/src/main/java/wdei/b1/TreeSimilarity.java
+++ tree-similarity/src/main/java/wdei/b1/TreeSimilarity.java
@@ -3,9 +3,30 @@ package wdei.b1;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class TreeSimilarity {
 
     private JaroDistance jaroDistance = new JaroDistance();
+
+
+    HashMap<String, String> tagCodes = new HashMap<String, String>();
+    private Stack<String> codes = new Stack<String>();
+
+    public TreeSimilarity() {
+        // !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}
+
+        for (int i = 122; i>=97; i--) {
+            codes.push("" + (char) i);
+        }
+        for (int i = 90; i>=65; i--) {
+            codes.push("" + (char) i);
+        }
+        for (int i = 57; i>=48; i--) {
+            codes.push("" + (char) i);
+        }
+    }
 
     public double calculate(Document left, Document right) {
         Element leftRoot = left.select("html").iterator().next();
@@ -15,15 +36,28 @@ public class TreeSimilarity {
         Element rightRoot = right.select("html").iterator().next();
         StringBuilder r = new StringBuilder();
         getPath(rightRoot, r);
-
-        return jaroDistance.calculate(l.toString(), r.toString());
+        System.out.println("compare:");
+        System.out.println(" > " + l.toString());
+        System.out.println(" > " + r.toString());
+        double res = jaroDistance.calculate(l.toString(), r.toString());
+        System.out.println(" = " + res);
+        return res;
     }
 
     private void getPath(Element tree, StringBuilder str) {
-        str.append(tree.nodeName());
+        str.append(getCode(tree.nodeName()));
         for (Element element : tree.children()) {
             getPath(element, str);
         }
+    }
+
+    private String getCode(String tagName) {
+        String code = tagCodes.get(tagName);
+        if (code == null) {
+            code = codes.pop();
+            tagCodes.put(tagName, code);
+        }
+        return code;
     }
 
 }
--- tree-similarity/src/test/java/wdei/b1/TreeSimilarityTest.java
+++ tree-similarity/src/test/java/wdei/b1/TreeSimilarityTest.java
@@ -3,6 +3,7 @@ package wdei.b1;
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -18,40 +19,81 @@ import java.io.IOException;
 public class TreeSimilarityTest {
 
     @Test
-    public void testDocs() {
+    public void testSimple() {
 
         String left = "<html><h1></h1><p></p></html>";
         String right = "<html><h1></h1><p></p></html>";
 
-        test(left, right);
-
-        left = "<html>" +
-                "<h1></h1>" +
-                "  <div>" +
-                "    <div>" +
-                "      <p></p><span></span>" +
-                "    </div>" +
-                "    <div>" +
-                "      <p></p><span></span>" +
-                "    </div>" +
-                "    <div>" +
-                "      <p></p><span></span>" +
-                "    </div>" +
-                "    <div>" +
-                "      <p></p><span></span>" +
-                "    </div>" +
-                "  </div>" +
-                "</html>";
-        right = "<html><h1></h1><p></p></html>";
-
-        test(left, right);
+        test("equal", left, right);
 
     }
 
-    private void test(String left, String right) {
+    @Test
+    public void testLongShort() {
+        String left = "<html>" +
+                "<h1></h1>" +
+                "  <div>" +
+                "    <div>" +
+                "      <p></p>" +
+                "      <span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p>" +
+                "      <span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p>" +
+                "      <span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p>" +
+                "      <span></span>" +
+                "    </div>" +
+                "  </div>" +
+                "</html>";
+        String right = "<html>" +
+                "<h1></h1>" +
+                "<p></p>" +
+                "</html>";
+
+        test("long-short", left, right);
+    }
+
+    @Test
+    public void testDifferent() {
+        String left = "<html>" +
+                "<h1></h1>" +
+                "  <div>" +
+                "    <div>" +
+                "      <p></p>" +
+                "      <span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p>" +
+                "      <span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p>" +
+                "      <span></span>" +
+                "    </div>" +
+                "    <div>" +
+                "      <p></p>" +
+                "      <span></span>" +
+                "    </div>" +
+                "  </div>" +
+                "</html>";
+        String right = "<html>" +
+                "<h1></h1>" +
+                "<p></p>" +
+                "</html>";
+
+        test("different", left, right);
+    }
+
+    private void test(String label, String left, String right) {
         Document leftDoc = Jsoup.parse(left);
         Document rightDoc = Jsoup.parse(right);
-        System.out.println("\n\n TEST");
+        System.out.println("\n\nTEST: " + label);
         System.out.println("node matches: " + new TreeSimilarity().calculate(leftDoc, rightDoc));
     }
 
@@ -59,7 +101,7 @@ public class TreeSimilarityTest {
     public void testWikiPages() throws IOException {
         String l = FileUtils.readFileToString(new File("src/test/resources/wikiFoo.html"));
         String r = FileUtils.readFileToString(new File("src/test/resources/wikiJaroWinkler.html"));
-        test(l, r);
+        test("equal", l, r);
     }
 
 }
